03ced2c6cf3054836f129d0a971a578c9e455e76
Piter <pomaranski.piotr@gmail.com>
2020-11-23 10:19:46 +0100

    add table and details edit/delete/details option/link

--- core/tables.py
+++ core/tables.py
@@ -1,10 +1,17 @@
-from django_tables2 import tables
-
+import django_tables2 as tables
 from core.models import DailyRoute
 
 
+ACTIONS = '''
+   <a href="{% url 'employee_update' record.pk %}">Edit</a>
+   <a href="{% url 'employee_detail' record.pk %}">View</a>
+'''
+
 class DailyRouteTable(tables.Table):
+    actions = tables.TemplateColumn(ACTIONS)
+
+
     class Meta:
         model = DailyRoute
         template_name = "django_tables2/bootstrap.html"
-        fields = ("start", 'end', 'date', 'distance',)
+        fields = ('date', "start", 'end', 'distance', 'driver', 'truck', 'order',)
