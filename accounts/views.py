407abecfdf96ae9d22878e7778b6e5c4a8ab10f9
ja <ja@cerber.pl>
2020-11-25 17:21:26 +0100

    add accounts/forms.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
-from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
+from django.urls import reverse_lazy
 
-from accounts.forms import SubmittableAuthenticationForm
+from accounts.forms import SubmittableAuthenticationForm, SubmittablePasswordChangeForm
 
 
 class SubmittableLoginView(LoginView):
     form_class = SubmittableAuthenticationForm
     template_name = 'form.html'
+
+
+class SubmittablePasswordChangeView(PasswordChangeView):
+    form_class = SubmittablePasswordChangeForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('index')
+
+
+class SuccessMessagedLogoutView(LogoutView):
+    def get_next_page(self):
+        result = super().get_next_page()
+        messages.success(self.request, 'Logout successful')
