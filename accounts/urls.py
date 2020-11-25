6d0615312c2bc943d6c1a90a0bf9bda090b89fbc
ja <ja@cerber.pl>
2020-11-25 20:13:55 +0100

    add 'logout', 'password-change', 'sign-up' into accounts.urls add SignUpView into accounts.views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
-from accounts.views import SubmittableLoginView, SuccessMessagedLogoutView, SubmittablePasswordChangeView
+from accounts.views import (SubmittableLoginView,
+                            SuccessMessagedLogoutView,
+                            SubmittablePasswordChangeView,
+                            SignUpView)
 from django.urls import path
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', SubmittableLoginView.as_view(), name='login'),
+    path('logout/', SuccessMessagedLogoutView.as_view(), name='logout'),
+    path('password-change/', SubmittablePasswordChangeView.as_view(), name='password_change'),
+    path('sing-up/', SignUpView.as_view(), name='sign_up'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-from accounts.forms import SubmittableAuthenticationForm, SubmittablePasswordChangeForm
+from accounts.forms import SubmittableAuthenticationForm, SubmittablePasswordChangeForm, SignUpForm
 
 
 class SubmittableLoginView(LoginView):
@@ -21,3 +22,9 @@ class SuccessMessagedLogoutView(LogoutView):
         result = super().get_next_page()
         messages.success(self.request, 'Logout successful')
         return result
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'forms.html'
+    success_url = reverse_lazy('index')
